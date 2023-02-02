5b084dbc07d6ce0d22510acb6f02f656f843a32d
wallas <whuanca06@gmail.com>
2023-02-02 15:28:49 -0400

    adding counter app

---
+++ 03_counter_app/src/FirstApp.tsx
@@ -0,0 +1,13 @@
+
+ const newMessage = 'wilson';
+export const FirstApp = () => {
+    
+    return(
+        <>
+        
+        <h3> {newMessage}</h3>
+        <p>parrafo de mrd</p>
+        
+        </>
+    )
+}
--- 03_counter_app/src/main.tsx
+++ 03_counter_app/src/main.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { FirstApp } from './FirstApp';
+import {App} from './HellloWorldApp'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+  const roots = ReactDOM.createRoot(document.getElementById('root')!);
+
+roots.render(<FirstApp/>);
